cc5170d4a703427ac0373a7f1f3768e887c23393
Santiago Andrés Comas Durán <santiagocomas07d@gmail.com>
2024-09-20 11:40:07 -0500

    AAC_PT_Logo_prueba_Detalles

--- Funciones_p.py
+++ Funciones_p.py
@@ -2,7 +2,7 @@
 
 
 
-# Funcion para calcular el 10%
+# Funcion para calcular el 10% 
 
 def procesoNotas(nNotas):
    
@@ -16,32 +16,29 @@ def procesoNotas(nNotas):
      return promedioR_1
 
 
-# Funcion para calcular el 30%
+# Funcion para calcular el 30%  
 
 def promedioCorte(promedio_1, promedio_2):
 
  resultado = ((promedio_1*0.34) + (promedio_2*0.66))
- print(resultado)
+ return resultado
  
 
 # Ojo faltan llos porcentajes fdel 10 y el 20 porciento
 
-def procesoMaterias(nMaterias):
+# def procesoMaterias(nMaterias):
    
-     lista = []
-     suma=0
-     promedioT=0
-     nGenerica= float(input(f"Ingresa la nota de tu prueba generica: "))
-     for i in range(nMaterias):
-         promedioF_1= float(input(f"Ingresa tu nota numero {i+1} : "))
-         promedioF_2= float(input(f"Ingresa tu nota sumativa numero {i+1} : "))
-         lista.insert(i,promedioF_1+promedioF_2)
-         promedioST=promedioST+lista[i]
-         promedioT=((promedioST+nGenerica)/(nMaterias+1))
-     return promedioT
+#      lista = []
+#      suma=0
+#      promedioT=0
+#      nGenerica= float(input(f"Ingresa la nota de tu prueba generica: "))
+#      for i in range(nMaterias):
+#          promedioF_1= float(input(f"Ingresa tu nota promedio 1 {i+1} : "))
+#          promedioF_2= float(input(f"Ingresa tu nota sumativa numero {i+1} : "))
+#          lista.insert(i,promedioF_1+promedioF_2)
+#          promedioST=promedioST+lista[i]
+#          promedioT=((promedioST+nGenerica)/(nMaterias))
+#      return promedioT
     
 
 
-def saludo():
-
-    print("Hola mundo")
--- Index.py
+++ Index.py
@@ -2,62 +2,89 @@
 
 
 
-from Funciones_p import promedioCorte ,saludo, procesoNotas, procesoMaterias
+from Funciones_p import promedioCorte, procesoNotas
 
 
 
 # Presentacion
-print("Bienvenido al software")
+print(" ")
+print("Bienvenido al software: Academic Average Calculator")
+print(" ")
+
+#Presentacion
+
+print("By:")
+print("---------------------------------------")
+print("|     -----  -----  ----   --  --     |")
+print("|     -      -   -  -   -  -    -     |")  
+print("|     -      -   -  -   -  -    -     |")
+print("|     - - -  - - -  - -     -  -      |")
+print("---------------------------------------")
+print(" ")
+
 
 # Menú de opciones
 print("Opcion 1: Promedio 30% -> Ingrese: Promedio 10% y Promedio 20%")
 print("Opcion 2: Promedio 10% y 30% -> Ingrese: Notas de actividades y tareas para hallar el 10% y posteriormente hallar el 30%")
-print("Opcion 3: Nota total del semestre")
+# print("Opcion 3: Nota total del semestre")
 
 
 # Respuesta del Usuario
-op = int(input("Haz tu eleccion "))
+print(" ")
+op = int(input("Por favor, selecciona una opción marcando el número correspondiente: "))
 
 # Condiciones
 # Calcular: Promedio 30%- Caso: Promedio del 10% y del 20%
-if op == 1:  
+if op == 1:
+    nombreMateria = input("Ingresa el nombre de la materia: ")
     promedio_1 = float(input("Ingresa tu nota del 10%: "))
     promedio_2 = float(input("Ingresa tu nota del 20%: "))
     if promedio_1 > 0 and promedio_2 >0:
+          if promedio_1 <= 5 and promedio_2 <=5:
 
-          resultadoOp = promedioCorte(promedio_1,promedio_2)
+            resultadoOp = promedioCorte(promedio_1,promedio_2)
 
-          print(f"El resultado es: {resultadoOp}")
+            print(f"El resultado correspondiente al promedio del corte de la materia {nombreMateria} es: {resultadoOp}")
+          else:   
+
+           print("Error --la nota maxima es 5.0--")      
     else:
-      print("Debes ingresar numeros positivos reales")
+       print("Debes ingresar numeros positivos reales")
 else:
 
     # Calcular: Promedio 30%- Caso: Promedio del 10% a partir de notas y el 20%
 
     if op == 2:
+        nombreMateria = input("Ingresa el nombre de la materia: ")
+        
         nNotas= int(input("Cuantas notas tienes? "))
-        promedio_1=procesoNotas(nNotas)
-        promedio_2=float(input("Ingresa tu nota del 20%: "))
-     
-        if promedio_1 > 0 and promedio_2 >0:
-         resultadoOp_2 = promedioCorte(promedio_1,promedio_2)
+        if nNotas > 0: 
 
-     
+         promedio_1=procesoNotas(nNotas)
+         promedio_2=float(input("Ingresa tu nota del 20%: "))
+    
+         if promedio_1 > 0 and promedio_2 >0:
+             if promedio_1 <= 5 and promedio_2 <=5:
 
-        else:
+                resultadoOp_2 = promedioCorte(promedio_1,promedio_2)
+
+                print(f"El resultado correspondiente al promedio del corte de la materia {nombreMateria} es: {resultadoOp_2}")
+             else:
+               print("Error --la nota maxima es 5.0--") 
+         else:
              print("Debes ingresar numeros positivos reales")
-    else:
+    # else:
 
-        if op == 3:
+    #     if op == 3:
             
-            nMaterias= int(input("Cuantas materias tienes"))
-            promedioTotal=procesoMaterias(nMaterias)
+    #         nMaterias= int(input("Cuantas materias tienes? "))
+    #         promedioTotal=procesoMaterias(nMaterias)
 
-            print(f"Promedio Total de tu semestre: {promedioTotal}")
+    #         print(f"Promedio Total de tu semestre: {promedioTotal}")
 
 
 
         #Cuantas materias tienes, de ahi se saca la cantidad de promedios
  
-        else:
+    else:
             print("Error, la funcion elegida no esta dentro de las opciones del menú")
